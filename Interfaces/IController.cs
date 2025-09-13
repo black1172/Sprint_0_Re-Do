c70c7d1bc1ec2e3cdd352f3074f8551014f4f684
Wyatt Black <black.1172@buckeyemail.osu.edu>
2025-09-12 21:36:35 -0400

    added interfaces and core class

---
+++ Interfaces/IController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IController
+{
+    public interface IController
+    {
+        void Update(Sprint4 game, IPlayer player);
+    }
+}
